204950ff8d45eed73703b604bee2c6403863338c
Matthew Fiske <matthew.fiske@snhu.edu>
2025-04-06 14:52:24 -0700

    Player widget syntax error fix

---
+++ Source/GAM312_Fiske_Survive/PlayerWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_FISKE_SURVIVE_API UPlayerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
